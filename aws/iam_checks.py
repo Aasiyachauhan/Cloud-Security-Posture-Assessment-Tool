50bc706baeb8ea7cc4527023ed3307e3ccbf6ce9
AasiyaChauhan <aasiyachauhan23@gmail.com>
2026-07-30 14:03:03 -0400

    Implemented IAM user discovery checks

--- aws/iam_checks.py
+++ aws/iam_checks.py
@@ -1,5 +1,25 @@
+from models.finding import Finding
+
+
 def run_iam_checks(session):
 
     findings = []
+
+    iam = session.client("iam")
+
+    response = iam.list_users()
+
+
+    for user in response["Users"]:
+
+        findings.append(
+            Finding(
+                "IAM",
+                f"IAM user found: {user['UserName']}",
+                "INFO",
+                "Review user permissions"
+            )
+        )
+
 
     return findings
